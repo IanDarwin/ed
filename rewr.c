d83c76014e85a11b8995d18e1ec23c7f8c33634f
IanDarwin <>
1994-02-07 13:12:51 +0000

    Use error() to make perror-like printfs. Check each fgets() and each fprintf() from/to the file. Use Geoff's preferred one-exit-per-function coding style.

--- rewr.c
+++ rewr.c
@@ -1,4 +1,5 @@
 /* dowrite -- write lines n1 through n2 to file */
+/* doread -- read file fn after line n1 */
 
 #include <stdio.h>
 #include <assert.h>
@@ -10,26 +11,30 @@ int n1, n2;
 char *fn;
 {
 	FILE *f;
-	int i;
+	int i, status = OK;
 	char line[MAXSTR];
 	int nch = 0, nli = 0;
 
 	if (n1 <= 0 || n1 > n2 || n2 > lastln)
-		return ERR;
-	if ((f = fopen(fn, "w")) == 0) {
-		strcpy(errmsg, "open for write failed");
-		return ERR;
-	} 
-	for (i = n1; i <= n2; i++) {
-		gettxt(i, line);
-		fprintf(f, "%s\n", line);
-		nch += strlen(line) + 1;
-		++nli;
+		status = ERR;
+	if ((f = fopen(fn, "w")) == 0)
+		status = error("open of %s for write failed", fn);
+	else { 
+		for (i = n1; i <= n2; i++) {
+			gettxt(i, line);
+			fprintf(f, "%s\n", line);
+			if (ferror(f)) {
+				status = error("Write error on %s", fn);
+				break;
+			}
+			nch += strlen(line) + 1;
+			++nli;
+		}
+		fclose(f);
+		printf("%u/%u %s\n", nch, nli, fn);
+		curln = n2;
 	}
-	fclose(f);
-	printf("%u/%u %s\n", nch, nli, fn);
-	curln = n2;
-	return OK;
+	return status;
 }
 
 int
@@ -38,27 +43,30 @@ int n1;
 char *fn;
 {
 	FILE *f;
-	int i;
+	int i, status = OK;
 	char line[MAXSTR];
 	int nch = 0, nli = 0;
 
 	if (n1 < 0 || n1 > lastln)
-		return ERR;
-
-	if ((f = fopen(fn, "r")) == 0) {
-		strcpy(errmsg, "open for read failed");
-		return ERR;
-	} 
-	curln = n1;
-	while (fgets(line, sizeof line -1, f) != NULL) {
-		i = strlen(line);
-		nch += i; nli++;
-		if (i)
-			line[i-1] = '\0';	/* chop \n */
-		puttxt(line);
+		status = ERR;
+	else if ((f = fopen(fn, "r")) == 0)
+		status = error("open of %s for read failed", fn);
+	else {
+		curln = n1;
+		while (fgets(line, sizeof line -1, f) != NULL) {
+			if (ferror(f)) {
+				status = error("Read error on %s", fn);
+				break;
+			}
+			i = strlen(line);
+			nch += i; nli++;
+			if (i)
+				line[i-1] = '\0';	/* chop \n */
+			puttxt(line);
+		}
+		fclose(f);
+		printf("%u/%u %s\n", nch, nli, fn);
+		assert(curln == n1 + nli);
 	}
-	fclose(f);
-	printf("%u/%u %s\n", nch, nli, fn);
-	assert(curln == n1 + nli);
-	return OK;
+	return status;
 }
